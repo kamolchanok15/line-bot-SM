e2151a31a8062cd095c0337b9d9d527cf9c050f6
kamolchanok15 <36261649+kamolchanok15@users.noreply.github.com>
2018-06-18 19:23:57 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ba9rGKeKDf4sG0FgHHBWso7qtLDQvLy//FXPKGq4m79N3sHVI8tljfRIKluHDG3hN+3bKovVFnAbbOBZMAM5pCgcqc7Wwo8/w0pGc6D0Zp/t/qqhxHW4veMR6UbPx44djxOw/QTIA+NBxYNCu9APvQdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
